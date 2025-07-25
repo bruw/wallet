e54efeba91072b5ad5f4ff644f2129f9ce71b7be
bruw <bwogt.dev@gmail.com>
2025-07-25 13:35:30 -0300

    create migration for additional user data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,10 +23,14 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = \Faker\Factory::create('pt_BR');
+
         return [
-            'name' => fake()->name(),
+            'name' => $faker->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'cpf' => $faker->unique()->cpf(false),
+            'phone' => $faker->unique()->cellPhoneNumber(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
