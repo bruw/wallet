7006050120f735b23059604e179150ab031288ae
bruw <bwogt.dev@gmail.com>
2025-07-26 13:56:02 -0300

    Added tests

---
+++ tests/Feature/Auth/Logout/LogoutAccessTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Auth\Logout;
+
+use App\Http\Messages\FlashMessage;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Laravel\Sanctum\Sanctum;
+
+class LogoutAccessTest extends LogoutTestSetUp
+{
+    public function test_the_user_must_be_logged_out_and_the_access_token_destroyed(): void
+    {
+        Sanctum::actingAs($this->user);
+        $this->user->withAccessToken($this->token->accessToken);
+
+        $this->deleteJson(route('api.auth.logout'))
+            ->assertOk()
+            ->assertJson(fn (AssertableJson $json) => $json->where('message.type', FlashMessage::SUCCESS)
+                ->where('message.text', trans('flash_messages.logout'))
+            );
+
+        $this->assertCount(0, $this->user->fresh()->tokens);
+    }
+}
